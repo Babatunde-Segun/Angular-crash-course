7bc36b947291e50e304c4c69db9dcb762ae57fcf
Babatunde-Segun <officiallyvincify@gmail.com>
2024-08-07 10:40:01 +0100

    implement pipes

--- src/app/crisis-list/crisis-list.component.ts
+++ src/app/crisis-list/crisis-list.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { DatePipe, UpperCasePipe, LowerCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-crisis-list',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, UpperCasePipe, LowerCasePipe],
   templateUrl: './crisis-list.component.html',
-  styleUrl: './crisis-list.component.css'
+  styleUrl: './crisis-list.component.css',
 })
 export class CrisisListComponent {
-
+  birthday = new Date().toLocaleDateString();
+  currentTime = new Date();
 }
