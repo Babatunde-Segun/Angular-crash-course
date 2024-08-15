ac3e050fd13d3fc8fa758f81f7bfc40b6e77a6e9
Babatunde-Segun <officiallyvincify@gmail.com>
2024-08-15 15:29:55 +0100

    implement custom pipe called reverse pipe

--- src/app/components/pipe/pipe.component.ts
+++ src/app/components/pipe/pipe.component.ts
@@ -8,6 +8,21 @@ import {
 import { Component } from '@angular/core';
 
 import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  standalone: true,
+  name: 'reverse',
+})
+export class ReversePipe implements PipeTransform {
+  transform(value: string): string {
+    let reverse = '';
+    for (let i = value.length - 1; i >= 0; i--) {
+      reverse += value[i];
+    }
+    return reverse;
+  }
+}
+
 @Pipe({
   standalone: true,
   name: 'star',
@@ -27,6 +42,7 @@ export class StarPipe implements PipeTransform {
     DatePipe,
     DecimalPipe,
     CurrencyPipe,
+    ReversePipe,
     StarPipe,
   ],
   templateUrl: './pipe.component.html',
@@ -36,6 +52,7 @@ export class PipeComponent {
   loudMessage = 'we think you are doing great!';
   todayDate = new Date();
   num = 103.1234;
+  word = 'You are a champion';
 
   starBoy = '5 star general';
 
