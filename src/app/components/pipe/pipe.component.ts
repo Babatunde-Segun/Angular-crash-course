bdff5efcdc688974a76a752686db4197b989a780
Babatunde-Segun <officiallyvincify@gmail.com>
2024-08-15 15:20:56 +0100

    implement decimal and currency  pipe

--- src/app/components/pipe/pipe.component.ts
+++ src/app/components/pipe/pipe.component.ts
@@ -1,14 +1,23 @@
-import { DatePipe, LowerCasePipe, UpperCasePipe } from '@angular/common';
+import {
+  CurrencyPipe,
+  DatePipe,
+  DecimalPipe,
+  LowerCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pipe',
   standalone: true,
-  imports: [UpperCasePipe, LowerCasePipe, DatePipe],
+  imports: [UpperCasePipe, LowerCasePipe, DatePipe, DecimalPipe, CurrencyPipe],
   templateUrl: './pipe.component.html',
   styleUrl: './pipe.component.css',
 })
 export class PipeComponent {
   loudMessage = 'we think you are doing great!';
   todayDate = new Date();
+  num = 103.1234;
+
+  cost = 4560.34;
 }
