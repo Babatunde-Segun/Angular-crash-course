330b6c02a726e6feae5d5f2522a7aead77acd04d
Babatunde-Segun <officiallyvincify@gmail.com>
2024-08-15 15:24:24 +0100

    implement custom pipe

--- src/app/components/pipe/pipe.component.ts
+++ src/app/components/pipe/pipe.component.ts
@@ -7,10 +7,28 @@ import {
 } from '@angular/common';
 import { Component } from '@angular/core';
 
+import { Pipe, PipeTransform } from '@angular/core';
+@Pipe({
+  standalone: true,
+  name: 'star',
+})
+export class StarPipe implements PipeTransform {
+  transform(value: string): string {
+    return `⭐️ ${value} ⭐️`;
+  }
+}
+
 @Component({
   selector: 'app-pipe',
   standalone: true,
-  imports: [UpperCasePipe, LowerCasePipe, DatePipe, DecimalPipe, CurrencyPipe],
+  imports: [
+    UpperCasePipe,
+    LowerCasePipe,
+    DatePipe,
+    DecimalPipe,
+    CurrencyPipe,
+    StarPipe,
+  ],
   templateUrl: './pipe.component.html',
   styleUrl: './pipe.component.css',
 })
@@ -18,6 +36,8 @@ export class PipeComponent {
   loudMessage = 'we think you are doing great!';
   todayDate = new Date();
   num = 103.1234;
+
+  starBoy = '5 star general';
 
   cost = 4560.34;
 }
