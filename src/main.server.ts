d7ffb0559b164093bba706fc7db31b095a4a8f01
Babatunde-Segun <officiallyvincify@gmail.com>
2024-08-29 23:49:18 +0100

    adding app module, but it is not working yet(animation)

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 import { appConfig } from './app/app.config';
+import { AppModule } from './app/app.module';
 
 const bootstrap = () => bootstrapApplication(AppComponent, appConfig);
 
