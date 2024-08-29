e428ba4fad80dd5ff80de6e30a56b94301c68b4c
Babatunde-Segun <officiallyvincify@gmail.com>
2024-08-29 16:16:05 +0100

    omo, wetin day sup with this animation na

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { PreloadAllModules, provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()],
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideAnimationsAsync(),
+  ],
 };
